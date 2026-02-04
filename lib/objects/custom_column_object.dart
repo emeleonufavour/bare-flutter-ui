d3916fb1aa0dfcce666c4d638d7c2b2d1aff2e05
Favour Emele-Onu <emeleonufavour15@gmail.com>
2026-02-04 14:32:03 +0100

    feat: support cross axis alignment for column

--- lib/objects/custom_column_object.dart
+++ lib/objects/custom_column_object.dart
@@ -7,40 +7,79 @@ class CustomColumnObject extends RenderBox
         ContainerRenderObjectMixin<RenderBox, FlexParentData>,
         RenderBoxContainerDefaultsMixin<RenderBox, FlexParentData>,
         DebugOverflowIndicatorMixin {
+  final CrossAxisAlignment _crossAxisAlignment;
+  CustomColumnObject({
+    CrossAxisAlignment crossAxisAlignment = CrossAxisAlignment.start,
+  }) : _crossAxisAlignment = crossAxisAlignment;
+
+  CrossAxisAlignment get crossAxisAlignment => _crossAxisAlignment;
+  set crossAxisAlignment(CrossAxisAlignment val) {
+    if (val != _crossAxisAlignment) {
+      markNeedsLayout();
+    }
+  }
+
   @override
   void setupParentData(covariant RenderObject child) {
-    if (child.parentData! is FlexParentData) {
+    if (child.parentData is! FlexParentData) {
       child.parentData = FlexParentData();
     }
+  }
+
+  double getXAxis(CrossAxisAlignment val, double maxWidth, double childWidth) {
+    double res = 0;
+    switch (val) {
+      case CrossAxisAlignment.center:
+        res = (maxWidth - childWidth) / 2;
+        break;
+
+      case CrossAxisAlignment.end:
+        res = maxWidth - childWidth;
+        break;
+
+      default:
+        return 0;
+    }
+
+    return res;
   }
 
   @override
   void performLayout() {
     double dy = 0.0;
+
     double maxWidth = 0.0;
 
     RenderBox? child = firstChild;
-    double remainingHeight = constraints.maxHeight;
 
+    // Let's get the maximum width of all the children
     while (child != null) {
-      final FlexParentData data = child.parentData as FlexParentData;
+      final parentData = child.parentData as FlexParentData;
 
       child.layout(
         BoxConstraints(
           maxWidth: constraints.maxWidth,
           minWidth: 0,
           minHeight: 0,
-          maxHeight: remainingHeight,
+          maxHeight: constraints.maxHeight,
         ),
         parentUsesSize: true,
       );
 
-      data.offset = Offset(0, dy);
+      maxWidth = max(maxWidth, child.size.width);
+      child = parentData.nextSibling;
+    }
+
+    child = firstChild;
+
+    while (child != null) {
+      final FlexParentData data = child.parentData as FlexParentData;
+
+      double xAxis = getXAxis(_crossAxisAlignment, maxWidth, child.size.width);
+
+      data.offset = Offset(xAxis, dy);
 
       dy += child.size.height;
-      remainingHeight -= child.size.height;
-
-      maxWidth = max(maxWidth, child.size.width);
 
       child = data.nextSibling;
     }
